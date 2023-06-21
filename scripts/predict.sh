7dbf247d196b0def5b8a4757bd89a64d2f2d19fb
Katharina Fluegel <katharina.fluegel@kit.edu>
2023-06-21 13:29:25 +0200

    Add enroot create to predict.sh

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -7,4 +7,7 @@ SCRIPT="${WORKSPACE}/Hackasaurus-Rex/scripts/predict.py"
 export TOMOUNT="${WORKSPACE},"
 export COMMAND="python -u ${SCRIPT} -c ${CONFIG}"
 
+echo "Initializing enroot container"
+enroot create -n pyxis_torch /hkfs/work/workspace/scratch/ih5525-E2/containers/hackasaurous.sqsh || echo "Container already initialized"
+
 sbatch "${WORKSPACE}/Hackasaurus-Rex/scripts/launch_job.sbatch"
