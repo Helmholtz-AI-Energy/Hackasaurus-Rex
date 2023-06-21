5667ec4c83b61fafc9b17911e9188a941c3c5d4e
Katharina Fluegel <katharina.fluegel@kit.edu>
2023-06-21 11:15:50 +0200

    Update prediction with comm setup

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,14 +5,19 @@ import pathlib
 
 import yaml
 
-from src.hackasaurus_rex.training import evaluation
+from hackasaurus_rex import comm
+from hackasaurus_rex.training import evaluation
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="Path to the config file", type=pathlib.Path)
+    parser.add_argument("-c", "--config", help="Path to the config file", type=pathlib.Path, required=True)
     arguments = parser.parse_args()
 
     with open(arguments.config, "r") as yaml_file:
         config = yaml.safe_load(yaml_file)
+
+    rank, size = comm.init_and_set_config_rank_size(config)
+    config["world_size"] = size
+    config["rank"] = rank
 
     evaluation(config)
