450791e7b71ea9f72bd3a865ddde713c1fec35ed
Katharina Fluegel <katharina.fluegel@kit.edu>
2023-06-21 10:36:32 +0200

    Add predict.sh and use env variable COMMAND in launch_job.sbatch

---
+++ scripts/predict.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WORKSPACE="/hkfs/work/workspace/scratch/ih5525-E2/"
+CONFIG="${WORKSPACE}/Hackasaurus-Rex/configs/detr_prot_predict.yml"
+SCRIPT="${WORKSPACE}/Hackasaurus-Rex/scripts/predict.py"
+
+export COMMAND="python -u ${SCRIPT} -c ${CONFIG}"}
+sbatch launch_job.sbatch
