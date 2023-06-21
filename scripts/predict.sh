6a98f1b629270cd4b1405b96e511531a6dc4a937
Katharina Fluegel <katharina.fluegel@kit.edu>
2023-06-21 11:16:10 +0200

    Fix predict scripts: $ and "

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -4,7 +4,7 @@ WORKSPACE=${WORKSPACE:-"/hkfs/work/workspace/scratch/ih5525-E2/"}
 CONFIG="${WORKSPACE}/Hackasaurus-Rex/configs/detr_prot_predict.yml"
 SCRIPT="${WORKSPACE}/Hackasaurus-Rex/scripts/predict.py"
 
-export $TOMOUNT="${WORKSPACE},"
+export TOMOUNT="${WORKSPACE},"
 export COMMAND="python -u ${SCRIPT} -c ${CONFIG}"
 
 sbatch "${WORKSPACE}/Hackasaurus-Rex/scripts/launch_job.sbatch"
