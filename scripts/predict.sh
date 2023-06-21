550b0cc71914290a826dabcd6ec14e30ee7dd2ca
Katharina Fluegel <katharina.fluegel@kit.edu>
2023-06-21 10:38:50 +0200

    Use variable workspace and make predict.sh executable

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-WORKSPACE="/hkfs/work/workspace/scratch/ih5525-E2/"
+WORKSPACE=${WORKSPACE:-"/hkfs/work/workspace/scratch/ih5525-E2/"}
 CONFIG="${WORKSPACE}/Hackasaurus-Rex/configs/detr_prot_predict.yml"
 SCRIPT="${WORKSPACE}/Hackasaurus-Rex/scripts/predict.py"
 
